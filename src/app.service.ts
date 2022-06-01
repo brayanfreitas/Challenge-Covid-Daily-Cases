65a580055f923aa2876a6f243723c0ef05366f86
Brayan Freitas <brayandeyvid17@gmail.com>
2022-06-01 18:47:45 -0300

    :sparkles: adding features

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getChallengeMessage(): string {
+    const challengeMessage = 'Backend Challenge 2021 🏅 - Covid Daily Cases';
+    return challengeMessage;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
